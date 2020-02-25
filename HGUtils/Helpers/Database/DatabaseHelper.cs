059ea28a8a97c6e4bbd1d251a2411a22a5bc9c77
Henry Gadea <henry_gadea@hotmail.com>
2020-02-25 00:15:03 -0600

    desarrollar helpers de base de datos para manejos de procedimientos almacenados y convertir una clase en datatable para enviarlo a base de datos

--- HGUtils/Helpers/Database/DatabaseHelper.cs
+++ HGUtils/Helpers/Database/DatabaseHelper.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using Dapper;
+using HGUtils.Common.Enums;
+using HGUtils.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace HGUtils.Helpers.Database
@@ -21,6 +28,75 @@ namespace HGUtils.Helpers.Database
                     transaction.Rollback();
                     throw;
                 }
+            }
+        }
+
+        public static SqlMapper.ICustomQueryParameter AsTableValuedParameter<T>(
+            this IEnumerable<T> enumerable,
+            string typeName,
+            IEnumerable<string> orderedColumnNames = null)
+        {
+            var dataTable = new DataTable();
+
+            if (typeof(T).IsValueType || typeof(T).FullName.Equals("System.String"))
+            {
+                dataTable.Columns.Add(orderedColumnNames == null ? "NONAME" : orderedColumnNames.First(), typeof(T));
+                foreach (T obj in enumerable)
+                {
+                    dataTable.Rows.Add(obj as object);
+                }
+            }
+            else
+            {
+                var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                var readableProperties = properties.Where(w => w.CanRead).ToArray();
+
+                if (readableProperties.Length > 1 && orderedColumnNames == null)
+                    throw new ArgumentException(@"Ordered list of column names must be provided when TVP contains more than one column");
+
+                var columnNames = (orderedColumnNames ?? readableProperties.Select(s => s.Name)).ToArray();
+
+                foreach (string name in columnNames)
+                {
+                    dataTable.Columns.Add(name, readableProperties.Single(s => s.Name.Equals(name)).PropertyType);
+                }
+
+                foreach (T obj in enumerable)
+                {
+                    dataTable.Rows.Add(columnNames
+                        .Select(s => readableProperties.Single(s2 => s2.Name.Equals(s)).GetValue(obj)).ToArray());
+                }
+            }
+
+            return dataTable.AsTableValuedParameter(typeName);
+        }
+
+        public static async Task UseStoredProcedureProcess<T>(
+            this T result,
+            string connectionString,
+            Func<SqlConnection, DynamicParameters, T, Task> func,
+            string resultCodePropertyName = "@RESULTCODE",
+            string resultMessagePropertyName = "@RESULTMESSAGE",
+            string resultDetailPropertyName = "@RESULTDETAIL")
+            where T : IResult
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var parameters = new DynamicParameters();
+                parameters.Add(resultCodePropertyName, null, DbType.Int32, ParameterDirection.Output);
+                parameters.Add(resultMessagePropertyName, null, DbType.String, ParameterDirection.Output, 500);
+                parameters.Add(resultDetailPropertyName, null, DbType.String, ParameterDirection.Output, 1000);
+
+                await func(connection, parameters, result);
+
+                result.Code = (int)(Enum.TryParse<ResultType>(
+                    parameters.Get<int>(resultCodePropertyName).ToString(), out var resultCode) ?
+                    resultCode :
+                    ResultType.DatabaseError);
+                result.Message = parameters.Get<string>(resultMessagePropertyName);
+                result.DetailMessage = parameters.Get<string>(resultDetailPropertyName);
             }
         }
     }
